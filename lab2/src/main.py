56f86c881d593b34613ef74f2c6aefcdc40ede4c
Ilya Vasiuk <ivasiuk@objectstyle.com>
2013-09-19 04:55:08 +0300

    Reconnectinon + fixup
    Now client can reconnect.
    Header was added. [Added python2, but program works with python3 too].
    Correct SIGTERM and SIGINT listeners were added.

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,4 +1,12 @@
-import socket, sys
+#!/usr/bin/env python3
+import socket, signal, sys
+
+def sigterm(signo, sigobj):
+    print("SIGTERM: {0} Exitting...".format(signo))
+    sys.exit()
+	
+signal.signal(signal.SIGTERM, sigterm)
+signal.signal(signal.SIGINT, sigterm)
 
 if len(sys.argv) == 3:
     host = sys.argv[1]
@@ -9,10 +17,12 @@ elif len(sys.argv) == 2:
 else:
     print("Usage: main.py [hostname] <port>")
     sys.exit()
+    
+buffsize = 4096
 	
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #option for correct socket closing
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #option for force socket reusing
     s.bind((host,port))
     s.listen(1)
 
@@ -20,14 +30,9 @@ except socket.error as msg:
     print("Failed to create a socket. Error #{0}: {1}".format(msg.errno, msg.strerror))
     sys.exit()
 
-try:
+while True:
     client, address = s.accept()
-    while True:
-        data = client.recv(4096)
-        if not data: 
-            break
-        else:
-            client.send(data)
-except KeyboardInterrupt:
-    s.close()
-    sys.exit()
+    data = client.recv(buffsize)
+    while data != b"":
+        client.send(data)
+        data = client.recv(buffsize)
