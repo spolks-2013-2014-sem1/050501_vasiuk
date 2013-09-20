8e6ba40d3947725f52ded9967d92839f28fc7757
Ilya Vasiuk <ivasiuk@objectstyle.com>
2013-09-20 15:27:38 +0300

    Multithreading
    Multithreading was added
    End of file is processed correctly now

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,10 +1,30 @@
-#!/usr/bin/env python3
-import socket, signal, sys
+#!/usr/bin/env python
+
+import socket, signal, sys, threading
+
+buffsize = 4096
+threads = []
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+
+    def run(self):    
+        data = self.socket.recv(2048)
+
+        while len(data):
+            self.socket.send(data)
+            data = self.socket.recv(2048)
+        self.socket.close()
 
 def sigterm(signo, sigobj):
     print("SIGTERM: {0} Exitting...".format(signo))
     sys.exit()
-	
+
 signal.signal(signal.SIGTERM, sigterm)
 signal.signal(signal.SIGINT, sigterm)
 
@@ -17,22 +37,22 @@ elif len(sys.argv) == 2:
 else:
     print("Usage: main.py [hostname] <port>")
     sys.exit()
-    
-buffsize = 4096
-	
+
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #option for force socket reusing
-    s.bind((host,port))
-    s.listen(1)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #option for force socket reusing
+    server.bind((host,port))
 
 except socket.error as msg:
     print("Failed to create a socket. Error #{0}: {1}".format(msg.errno, msg.strerror))
     sys.exit()
 
 while True:
-    client, address = s.accept()
-    data = client.recv(buffsize)
-    while data != b"":
-        client.send(data)
-        data = client.recv(buffsize)
+    server.listen(5)
+    (client, (ip, port)) = server.accept()
+    newthread = ClientThread(ip, port, client)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
