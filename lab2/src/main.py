95a974305c198ab99ddef6d34034aa1e48aa86f9
Ilya Vasiuk <ivasiuk@objectstyle.com>
2013-11-14 14:57:14 +0300

    return to single thread

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,30 +1,10 @@
-#!/usr/bin/env python2
-
-import socket, signal, sys, threading
-
-buffsize = 4096
-threads = []
-
-class ClientThread(threading.Thread):
-
-    def __init__(self, ip, port, socket):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.socket = socket
-
-    def run(self):    
-        data = self.socket.recv(2048)
-
-        while len(data):
-            self.socket.send(data)
-            data = self.socket.recv(2048)
-        self.socket.close()
+#!/usr/bin/env python3
+import socket, signal, sys
 
 def sigterm(signo, sigobj):
     print("SIGTERM: {0} Exitting...".format(signo))
     sys.exit()
-
+	
 signal.signal(signal.SIGTERM, sigterm)
 signal.signal(signal.SIGINT, sigterm)
 
@@ -37,7 +17,9 @@ elif len(sys.argv) == 2:
 else:
     print("Usage: main.py [hostname] <port>")
     sys.exit()
-
+    
+buffsize = 1024
+	
 try:
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #option for force socket reusing
@@ -47,12 +29,12 @@ except socket.error as msg:
     print("Failed to create a socket. Error #{0}: {1}".format(msg.errno, msg.strerror))
     sys.exit()
 
-while True:
-    server.listen(5)
-    (client, (ip, port)) = server.accept()
-    newthread = ClientThread(ip, port, client)
-    newthread.start()
-    threads.append(newthread)
+server.listen(5)
 
-for t in threads:
-    t.join()
+while True:
+    client, address = server.accept()
+    data = client.recv(buffsize)
+    while data != b"":
+        client.send(data)
+        data = client.recv(buffsize)
+    client.close()
